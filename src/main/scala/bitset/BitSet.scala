7370af066818ff61883d638ebd93f2e3b519cc10
jcz2 <jcz2@users.noreply.github.com>
2022-12-31 21:48:39 +0100

    init

---
+++ src/main/scala/bitset/BitSet.scala
@@ -0,0 +1,79 @@
+package bitset
+
+import scala.collection.mutable.ArrayBuffer
+
+class BitSet {
+  private val ar: ArrayBuffer[Long] = ArrayBuffer()
+
+  private def offset(i: Int): (Int, Int) = (i / 64, i % 64)
+
+  private def expand(i: Int): Unit = {
+    if (i >= ar.size) {
+      var d = i - ar.size + 1
+      while (d > 0) {
+        ar.append(0)
+        d -= 1
+      }
+    }
+  }
+
+  def set(v: Int): Unit = {
+    val (i, n) = offset(v)
+    expand(i)
+    ar(i) = ar(i) | (1L << n)
+  }
+
+  def get(v: Int): Boolean = {
+    val (i, n) = offset(v)
+    val result = ar(i) & (1L << n)
+    result != 0
+  }
+
+  def clear(v: Int): Unit = {
+    val (i, n) = offset(v)
+    ar(i) = ar(i) & (~(1L << n))
+  }
+
+  def cardinality(): Int = {
+    ar.foldLeft(0)((acc, v) => acc + java.lang.Long.bitCount(v))
+  }
+
+  def and(s: BitSet): Unit = {
+    if (ar.size > s.ar.size) {
+      for (i <- s.ar.size until ar.size) {
+        ar(i) = 0L
+      }
+    }
+    val min = Math.min(ar.size, s.ar.size)
+    for (i <- 0 until min) {
+      ar(i) = ar(i) & s.ar(i)
+    }
+  }
+
+  def or(s: BitSet): Unit = {
+    if (ar.size < s.ar.size) {
+      for (i <- ar.indices) {
+        ar(i) = ar(i) | s.ar(i)
+      }
+      for (i <- ar.size until s.ar.size) {
+        ar.append(s.ar(i))
+      }
+    } else {
+      for (i <- s.ar.indices) {
+        ar(i) = ar(i) | s.ar(i)
+      }
+    }
+  }
+
+  def flip(v: Int): Unit = {
+    val (i, n) = offset(v)
+    expand(i)
+    ar(i) = ar(i) ^ (1L << n)
+  }
+
+  def toLongArray: Array[Long] = ar.toArray.clone()
+
+  override def toString: String = {
+    ar.toString()
+  }
+}
---
+++ src/test/scala/BitSetSpec.scala
@@ -0,0 +1,120 @@
+import bitset.BitSet
+import org.scalacheck._
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.Configuration
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+import java.util
+
+class BitSetSpec extends AnyFunSpec with ScalaCheckPropertyChecks with Matchers with Configuration{
+
+  describe("BitSet") {
+    val gen = Gen.choose(0, 1000)
+
+    describe("get") {
+      it("should return proper value") {
+        val bsq = new BitSet()
+        val bs = new util.BitSet()
+
+        forAll(gen, minSuccessful(1000)) { v =>
+          bsq.set(v)
+          bs.set(v)
+          bsq.get(v) shouldEqual bs.get(v)
+          bsq.toLongArray shouldEqual bs.toLongArray
+        }
+      }
+    }
+
+    describe("and") {
+      it("should return proper value") {
+        val listGen = for (len <- gen; l <- Gen.listOfN(len, gen)) yield l
+        val pairGen = for (l1 <- listGen; l2 <- listGen) yield (l1, l2)
+
+        forAll(pairGen, minSuccessful(1000)) { case (l1, l2) =>
+          val bs1 = new util.BitSet()
+          val bs2 = new util.BitSet()
+          val bsq1 = new BitSet()
+          val bsq2 = new BitSet()
+
+          l1.foreach(v => {
+            bs1.set(v)
+            bsq1.set(v)
+          })
+
+          l2.foreach(v => {
+            bs2.set(v)
+            bsq2.set(v)
+          })
+
+          bs1.and(bs2)
+          bsq1.and(bsq2)
+
+          bs1.toLongArray.sum shouldEqual bsq1.toLongArray.sum
+          bs2.toLongArray.sum shouldEqual bsq2.toLongArray.sum
+        }
+      }
+    }
+
+    describe("or") {
+      it("should return proper value") {
+        val listGen = for (len <- gen; l <- Gen.listOfN(len, gen)) yield l
+        val pairGen = for (l1 <- listGen; l2 <- listGen) yield (l1, l2)
+
+        forAll(pairGen, minSuccessful(1000)) { case (l1, l2) =>
+          val bs1 = new util.BitSet()
+          val bs2 = new util.BitSet()
+          val bsq1 = new BitSet()
+          val bsq2 = new BitSet()
+
+          l1.foreach(v => {
+            bs1.set(v)
+            bsq1.set(v)
+          })
+
+          l2.foreach(v => {
+            bs2.set(v)
+            bsq2.set(v)
+          })
+
+          bs1.or(bs2)
+          bsq1.or(bsq2)
+
+          bs1.toLongArray.sum shouldEqual bsq1.toLongArray.sum
+          bs2.toLongArray.sum shouldEqual bsq2.toLongArray.sum
+        }
+      }
+    }
+
+    describe("flip") {
+      it("should return proper value") {
+        val bsq = new BitSet()
+        val bs = new util.BitSet()
+
+        forAll(gen, minSuccessful(1000)) { v =>
+          bsq.flip(v)
+          bs.flip(v)
+          bsq.toLongArray shouldEqual bs.toLongArray
+        }
+      }
+    }
+
+    describe("clear") {
+      it("should return proper value") {
+        val bsq = new BitSet()
+        val bs = new util.BitSet()
+
+        Gen.listOfN(1000, gen).sample.foreach(_.foreach(v => {
+          bsq.set(v)
+          bs.set(v)
+        }))
+
+        forAll(gen, minSuccessful(1000)) { v =>
+          bsq.clear(v)
+          bs.clear(v)
+          bsq.toLongArray shouldEqual bs.toLongArray
+        }
+      }
+    }
+  }
+}
